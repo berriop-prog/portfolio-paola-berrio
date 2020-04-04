0d2212ef65d0a13e3db5a5e5c64c6078c55a61f5
pberrio <berriop@gmail.com>
2020-04-04 17:54:04 -0500

    creado basico de componentes, paginas y enrutamiento de la app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Projects from './pages/projects/Projects';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/projects' component={Projects} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1> Paola Berrío's portfolio </h1>
+            <div className='header-options'>
+                <Link className='option' to='/'>About</Link>
+                <Link className='option' to='/contact'>Contact</Link>
+                <Link className='option' to='/projects'>Projects</Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
