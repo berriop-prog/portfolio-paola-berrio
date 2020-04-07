9ae80ff8ab7601564067fb3fbe37325acd006bff
pberrio <berriop@gmail.com>
2020-04-07 12:36:32 -0500

    se aguego las direcciones de las imagenas de los proyectos

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,58 +1,80 @@
 const projectsData = [
-    {
-      title: 'Landing page',
-      text: 'Tercer trabajo experimentando con HTML y CSS.',
-      projectName: 'LandingPage',
-      imgName: '3LandingPageFCC.png?alt=media&token=9a10d226-1d1f-45e8-a910-02b771b666d5' ,
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'tributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'proyectoTributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'proyectoTributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'proyectoTributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'proyectoTributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'proyectoTributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
-    {
-      title: 'Landing page',
-      text: 'Tercer trabajo experimentando con HTML y CSS.',
-      projectName: 'LandingPage',
-      imgName: '3LandingPageFCC.png?alt=media&token=9a10d226-1d1f-45e8-a910-02b771b666d5' ,
-    },
-    {
-      title: 'Tribute Page',
-      text: 'Primer trabajo de clase realizado en febrero de 2020.',
-      projectName: 'tributoRatas',
-      imgName: '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
-    },
+  {
+    title: 'Pomodoro con React',
+    text: '10',
+    projectName: 'LandingPage',
+    imgName:
+      '9pomodoro.png?alt=media&token=e78e559e-6b72-40fd-a11f-86413246222d',
+  },
+  {
+    title: 'Calculator con React',
+    text: '9',
+    projectName: 'LandingPage',
+    imgName:
+      '8calculator.png?alt=media&token=a4196f85-f88f-43b0-91b0-a0b05400e5cb',
+  },
+  {
+    title: 'Random Quote Machine con React',
+    text: '8 ',
+    projectName: 'tributoRatas',
+    imgName:
+      '7random.png?alt=media&token=aaa00a1e-8403-40a7-8d74-82f5287ac213',
+  },
+  {
+    title: 'Title-Case-a-SentencePassed',
+    text:
+      '7 Trabajo colaborativo con Yeraldith Camargo Ayala de FCC y AcademiaGeek educación virtural por Covid-19',
+    projectName: 'Title-Case-a-SentencePassed',
+    imgName:
+      'TrabajoPareja.png?alt=media&token=5d33d980-326f-4928-8ecd-2effc2eacfb5',
+  },
+  {
+    title: 'Tribute Page',
+    text: '6 primer portafolio',
+    projectName: 'Portafolio',
+    imgName:
+      '6portafolio.png?alt=media&token=91bf8125-ca8e-44e5-9b1f-3533544a9c50',
+  },
+  {
+    title: 'Trabajo en equipo',
+    text:
+      '5 Proyecto de clase en equipos con realizado con HTML, CSS y Bootstrap.',
+    projectName: 'Reto-1',
+    imgName:
+      '5reto1.png?alt=media&token=077f3a2a-edac-4495-8214-ced50caa9a0a',
+  },
+  {
+    title: 'Technical Documentation Page',
+    text:
+      '4 trabajo de clase realizado con HTML, CSS y Bootstrap con los parámetros de FreeCodeCamp',
+    projectName: 'DocumentPage',
+    imgName:
+      '4documentPage.png?alt=media&token=3c1a5344-96fd-4475-bf2c-e8e94c568655',
+  },
+  {
+    title: 'Landing page',
+    text:
+      '3 trabajo de clase realizado con HTML y CSS con los parámetros de FreeCodeCamp',
+    projectName: 'LandingPage',
+    imgName:
+      '3LandingPageFCC.png?alt=media&token=9a10d226-1d1f-45e8-a910-02b771b666d5',
+  },
+  {
+    title: 'Form',
+    text:
+      '2 trabajo de clase realizado con HTML y CSS con los parámetros de FreeCodeCamp.',
+    projectName: 'FormFCC',
+    imgName:
+      '2FormFCC.png?alt=media&token=a978d905-7b5d-46e1-9d2f-6d75b0525ff2',
+  },
+  {
+    title: 'Tribute Page',
+    text:
+      '1 trabajo de clase realizado con HTML y CSS con los parámetros de FreeCodeCamp.',
+    projectName: 'tributoRatas',
+    imgName:
+      '1FCCtributoRatas.png?alt=media&token=620d505e-1f71-4311-a1a1-97b11355f29b',
+  },
 ];
 
 export default projectsData;
